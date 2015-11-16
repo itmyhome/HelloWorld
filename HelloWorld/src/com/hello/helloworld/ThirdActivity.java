36eda1d69991af702d68a4cb75087993ae204c4e
itmyhome@126.com <itmyhome@126.com>
2015-11-17 00:31:43 +0800

    更多隐式调用的方法 浏览方式选择及拨打电话

--- HelloWorld/src/com/hello/helloworld/ThirdActivity.java
+++ HelloWorld/src/com/hello/helloworld/ThirdActivity.java
@@ -1,8 +1,6 @@
 package com.hello.helloworld;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,35 +11,21 @@ import android.widget.Button;
 import android.widget.Toast;
 
 /**
- * @Title: FirstActivity.java
+ * @Title: SecondActivity.java
  * @Package com.hello.helloworld
  * @Description: TODO(用一句话描述该文件做什么)
  * @author itmyhome
  * @date 2015年11月5日 下午10:12:33
  * @version V1.0
  */
-public class FirstActivity extends Activity {
+public class ThirdActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.first_layout);
-		Button button1 = (Button) findViewById(R.id.button1);
-
-		button1.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-//				Toast.makeText(FirstActivity.this, "you click button1",
-//						Toast.LENGTH_SHORT).show();
-				//销毁一个活动
-//				finish();
-				Intent intent = new Intent(Intent.ACTION_DIAL);
-//				intent.addCategory("com.hello.helloworld.MY_CATEGORY");
-				intent.setData(Uri.parse("tel:10086"));
-				startActivity(intent);
-			}
-		});
+		setContentView(R.layout.second_layout);
+		
 	}
 
 	@Override
@@ -56,11 +40,11 @@ public class FirstActivity extends Activity {
 
 		switch (item.getItemId()) {
 		case R.id.add_item:
-			Toast.makeText(FirstActivity.this, "you click add_item",
+			Toast.makeText(ThirdActivity.this, "you click add_item",
 					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.remove_item:
-			Toast.makeText(FirstActivity.this, "you click remove_item",
+			Toast.makeText(ThirdActivity.this, "you click remove_item",
 					Toast.LENGTH_SHORT).show();
 			break;
 		default:
